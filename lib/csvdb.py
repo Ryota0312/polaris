3c5b3082260df006ef1b21a83444574c8e883608
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-10-28 15:13:05 +0900

    Fix critical bug in db
    WDを使用した時期の保存にバグがあった．
    具体的には，毎回updateのたびに上書きされていた．
    このため，追記するように修正した．

--- lib/csvdb.py
+++ lib/csvdb.py
@@ -1,6 +1,9 @@
 import csv
 import re
 from pathlib import Path
+import datetime
+#TODO: 現状1列目がラベル，他2列がデータとなっているが汎用的にしたほうが良い
+###### たとえば，ヘッダを作ってそれをキーとする辞書で保持する
 
 class Database:
     def __init__(self, filename):
@@ -12,14 +15,7 @@ class Database:
     def __read(self):
         ret = []
         with open(self.filename, "r") as f:
-            #return [line.replace("\n", "").split(",", 1) for line in f]
-            #for line in f:
-            #    l = line.replace("\n", "").split(",", 1)
-            #    l2 = l[1].rsplit(",", 1)
-            #    ret.append([l[0], l2[0], l2[1]])
-            #return ret
             for i, line in enumerate(f):
-                #if i==0: continue
                 m = re.match("(.*),(\[.*\]),(\[.*\])", line)
                 ret.append([m.group(1), m.group(2), m.group(3)])
             return ret
@@ -30,20 +26,20 @@ class Database:
             
     def update(self, label, new_data1, new_data2):
         update_flag = 0
-        for d in self.data:
+        for i,d in enumerate(self.data):
             if d[0]==label:
-                d[1] = new_data1
-                d[2] = new_data2
+                self.data[i][1] = new_data1
+                self.data[i][2] = new_data2
                 update_flag = 1
         if update_flag == 0:
             self.data.append([label, new_data1, new_data2])
         self.__write_csv()
 
-    def get(self, label):
+    def get(self, label, row):
         for d in self.data:
             if d[0] == label:
                 try:
-                    return eval(d) #evalするかどうかは使い手が決めるべきかも
+                    return eval(d[row]) #evalするかどうかは使い手が決めるべきかも
                 except:
                     return []
         return [] # None を返して処理は他に任せたほうが良い
