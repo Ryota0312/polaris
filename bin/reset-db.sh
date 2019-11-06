afb1f79eb949cd58884aaced5a1c618a5fba3ce5
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-11-06 16:26:41 +0900

    Add reset-db script

---
+++ bin/reset-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SH_PATH=$(dirname $0)
+
+read -p "rm log/* db/* log.pickle OK? (y/N): " yn
+
+case "$yn" in
+    [yY]*)
+	rm $SH_PATH/../log/*
+	rm $SH_PATH/../db/*
+	rm $SH_PATH/../log.lspickle
+	echo "rm log/* db/* log.pickle"
+    ;;
+    *) echo "Canceled";;
+esac
