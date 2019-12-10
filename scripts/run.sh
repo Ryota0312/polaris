9ce7a7ec99519e3fa64550684c8ff6696e20d7e6
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-12-10 11:28:35 +0900

    Remove my run.sh

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/home/ryota/.local/share/virtualenvs/VFGen-jPfAPZdh/bin/python /home/ryota/Project/research/VFGen/bin/subaru.py update
-/home/ryota/.local/share/virtualenvs/VFGen-jPfAPZdh/bin/python /home/ryota/Project/research/VFGen/bin/subaru.py create
